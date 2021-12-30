e3736351b2b5d6c69bb79ff6ca06e90af8dab90e
Steve Brown <stephen.brown@konghq.com>
2021-12-30 18:03:57 +0000

    updated to add 2.7 build

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 repo="https://registry.hub.docker.com/v1/repositories/kong/kong-gateway/tags"
-declare -a versions=("2.4" "2.5" "2.6")
+declare -a versions=("2.4" "2.5" "2.6" "2.7")
 
-for i in "${versions[@]}"
-do
-  echo "$i"
-  KONG_VERSION=$(wget -q ${repo} -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' |grep $i |grep -v '-'|sort -n | tail -n 1)
-  echo "KONG_VERSION ${KONG_VERSION}"
-  KONG_VERSION=${KONG_VERSION} make 
+for i in "${versions[@]}"; do
+	echo "$i"
+	KONG_VERSION=$(wget -q ${repo} -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F: '{print $3}' | grep $i | grep -v '-' | sort -n | tail -n 1)
+	echo "KONG_VERSION ${KONG_VERSION}"
+	KONG_VERSION=${KONG_VERSION} make
 done
